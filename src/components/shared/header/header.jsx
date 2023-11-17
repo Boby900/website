18d80b068762c46831a2e2b2e29ca9d022ed0f05
Thi Van Le <vannnyle@gmail.com>
2023-11-17 18:45:11 +0100

    feat: keyboard navigation on home page

--- src/components/shared/header/header.jsx
+++ src/components/shared/header/header.jsx
@@ -19,6 +19,92 @@ import Github from './images/header-github.inline.svg';
 
 const Search = dynamic(() => import('components/shared/search'), { ssr: false });
 
+const SubMenuItem = ({ to, text, items, isThemeBlack }) => {
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const Tag = to ? Link : 'button';
+
+  return (
+    <li
+      className={clsx(items?.length > 0 && 'relative')}
+      aria-expanded={items?.length > 0 && isSubmenuOpen}
+      aria-haspopup={items?.length > 0}
+      role="menuitem"
+      onMouseEnter={() => setIsSubmenuOpen(true)}
+      onMouseLeave={() => setIsSubmenuOpen(false)}
+      onFocus={() => setIsSubmenuOpen(true)}
+      onBlur={() => setIsSubmenuOpen(false)}
+    >
+      <Tag
+        className={clsx(
+          'whitespace-pre',
+          items?.length > 0 &&
+            'relative pr-3.5 leading-none transition-colors duration-200 before:absolute before:right-0 before:top-[7px] before:h-0 before:w-0 before:border-4 before:border-transparent before:transition-[border-color] before:duration-200 hover:text-primary-2 group-hover:before:border-t-primary-2 dark:before:border-transparent after:absolute after:-inset-2',
+          items?.length > 0 && isThemeBlack
+            ? 'before:border-t-white'
+            : 'before:border-t-black dark:before:border-t-white',
+          isThemeBlack ? 'text-white' : 'text-black dark:text-white'
+        )}
+        to={to}
+        theme={isThemeBlack && to ? 'white' : 'black'}
+      >
+        {text}
+      </Tag>
+      {items?.length > 0 && (
+        <div
+          className={clsx(
+            'absolute bottom-0 translate-y-full pt-4 transition-[opacity,visibility] duration-200',
+            isSubmenuOpen ? 'visible opacity-100' : 'invisible opacity-0'
+          )}
+        >
+          <ul
+            className="min-w-[240px] rounded-2xl bg-white p-3.5"
+            style={{ boxShadow: '0px 4px 10px rgba(26, 26, 26, 0.2)' }}
+          >
+            {items.map(({ icon, text, description, to }, index) => (
+              <li
+                className={clsx(index !== 0 && 'mt-3.5 border-t border-t-gray-6 pt-3.5')}
+                key={index}
+              >
+                <Link className="flex items-center whitespace-nowrap hover:text-primary-2" to={to}>
+                  <img
+                    src={icon}
+                    alt="text"
+                    width={44}
+                    height={44}
+                    className="h-11 w-11 shrink-0"
+                    loading="lazy"
+                    aria-hidden
+                  />
+                  <span className="ml-3">
+                    <span className="t-xl block font-semibold !leading-none transition-colors duration-200">
+                      {text}
+                    </span>
+                    <span className="mt-1.5 block leading-none text-black">{description}</span>
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </li>
+  );
+};
+
+SubMenuItem.propTypes = {
+  to: PropTypes.string,
+  text: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      icon: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      to: PropTypes.string.isRequired,
+    })
+  ),
+  isThemeBlack: PropTypes.bool.isRequired,
+};
+
 const Header = ({
   className = null,
   theme,
@@ -67,68 +153,9 @@ const Header = ({
 
           <nav className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
             <ul className="flex space-x-12 3xl:space-x-10 2xl:space-x-8 lg:hidden">
-              {MENUS.header.map(({ to, text, items }, index) => {
-                const Tag = to ? Link : 'button';
-                return (
-                  <li className={clsx(items?.length > 0 && 'group relative')} key={index}>
-                    <Tag
-                      className={clsx(
-                        'whitespace-pre',
-                        items?.length > 0 &&
-                          'relative pr-3.5 leading-none transition-colors duration-200 before:absolute before:right-0 before:top-[7px] before:h-0 before:w-0 before:border-4 before:border-transparent before:transition-[border-color] before:duration-200 hover:text-primary-2 group-hover:before:border-t-primary-2 dark:before:border-transparent',
-                        items?.length > 0 && isThemeBlack
-                          ? 'before:border-t-white'
-                          : 'before:border-t-black dark:before:border-t-white',
-                        isThemeBlack ? 'text-white' : 'text-black dark:text-white'
-                      )}
-                      to={to}
-                      theme={isThemeBlack && to ? 'white' : 'black'}
-                    >
-                      {text}
-                    </Tag>
-                    {items?.length > 0 && (
-                      <div className="group-hover:opacity-1 invisible absolute bottom-0 translate-y-full pt-4 opacity-0 transition-[opacity,visibility] duration-200 group-hover:visible group-hover:opacity-100">
-                        <ul
-                          className="min-w-[240px] rounded-2xl bg-white p-3.5"
-                          style={{ boxShadow: '0px 4px 10px rgba(26, 26, 26, 0.2)' }}
-                        >
-                          {items.map(({ icon, text, description, to }, index) => (
-                            <li
-                              className={clsx(
-                                index !== 0 && 'mt-3.5 border-t border-t-gray-6 pt-3.5'
-                              )}
-                              key={index}
-                            >
-                              <Link
-                                className="flex items-center whitespace-nowrap hover:text-primary-2"
-                                to={to}
-                              >
-                                <img
-                                  src={icon}
-                                  alt="text"
-                                  width={44}
-                                  height={44}
-                                  className="h-11 w-11 shrink-0"
-                                  loading="lazy"
-                                  aria-hidden
-                                />
-                                <span className="ml-3">
-                                  <span className="t-xl block font-semibold !leading-none transition-colors duration-200">
-                                    {text}
-                                  </span>
-                                  <span className="mt-1.5 block leading-none text-black">
-                                    {description}
-                                  </span>
-                                </span>
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    )}
-                  </li>
-                );
-              })}
+              {MENUS.header.map((item, index) => (
+                <SubMenuItem {...item} isThemeBlack={isThemeBlack} key={index} />
+              ))}
             </ul>
           </nav>
 
